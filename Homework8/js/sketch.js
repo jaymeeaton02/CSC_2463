71e73f904a9ed65b7542ab59c22744b57ce80ef4
Jayme Eaton <77986526+jaymeeaton02@users.noreply.github.com>
2023-04-20 16:22:50 -0500

    Add files via upload

--- Homework8/js/sketch.js
+++ Homework8/js/sketch.js
@@ -5,42 +5,64 @@ let z = 0;
 let sound1 = new Tone.Player("sound1/Paint.wav");
 let sound2 = new Tone.Player("sound2/Dip.wav")
 
+let port;
+let writer, reader;
+let sliderLED;
+let xAxis, yAxia;
+let joySwitch;
+let sensorData = {};
+
+const encoder = new TextEncoder();
+const decoder = new TextDecoder();
+
 function setup() 
 {
-  createCanvas(600, 600);
-  background(255);
+ createCanvas(600, 600);
 
-  sound1.toDestination();
-  sound2.toDestination();
+ if ("serial" in navigator) 
+ {
+   let button = createButton("Connect");
+   button.position(580, 10);
+   button.mousePressed(connect);
 
-  const notes = ["C4", "E3", "G4", "F4", "A4", "C4", "G4", "A3", "D4", "G4", "B3", "F4"];
-  const durations = [0.1, 0.1, 0.1, 0.4, 0.4, 0.4, 0.7, 0.7, 0.7, 1, 1, 1];
-
-  const synth = new Tone.Synth().toDestination();
-  const musicPart = new Tone.Sequence((time, noteIndex) => 
-  {
-    synth.triggerAttackRelease(notes[noteIndex], durations[noteIndex], time);
-  },
-  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
-  "4n"
-  );
-
-  Tone.Transport.start();
-  musicPart.loop = true;
-  musicPart.start(0);
+   sliderLED = createSlider(0, 255, 0);
+   sliderLED.position(250, 600);
+   sliderLED.style("width", "200px");
+ }
 }
 
 function draw() 
 {
+  background(255);
+
+  if (reader) 
+  {
+    serialRead();
+  }
+
+  if (writer) 
+  {
+    writer.write(encoder.encode(xAxis + "," + yAxis + "," + joySwitch + "\n"))
+    writer.write(new Uint8Array([ sliderLED.value() ]));
+  }
+
+  joySwitch = sensorData.Switch;
+  xAxis = sensorData.Xaxis;
+  yAxis = sensorData.Yaxis;
+
+  //text("Joystick Switch: " + sensorData.Switch, 150, 150);
+  //text("Joystick X-axis: " + sensorData.Xaxis, 150, 175);
+  //text("Joystick Y-axis: " + sensorData.Yaxis, 150, 200);
+
+  push();
+  noFill();
+  circle(map(xAxis, 0, 255, 0, width), map(yAxis, 0, 255, 0, height), 10);
+  pop();
+
   textSize(17);
   textAlign(CENTER);
   text('Press Paint to initialize audio!', 300, 585);
   fill(0);
-
-  frameRate(1000);
-  strokeWeight(1);
-  stroke(255, 255, 255);
-  noFill();
 
   fill("red");
   rect(0, 0, 50, 50);
@@ -64,75 +86,125 @@ function draw()
   rect(0, 450, 50, 50);
 }
 
-function mouseDragged()
+
+async function serialRead() 
+{
+ while (true) 
+ {
+   const { value, done } = await reader.read();
+   if (done) 
+   {
+     reader.releaseLock();
+     break;
+   }
+   sensorData = JSON.parse(value);
+ }
+}
+
+
+async function connect() 
+{
+ port = await navigator.serial.requestPort();
+ await port.open({ baudRate: 9600 });
+ writer = port.writable.getWriter();
+ reader = port.readable
+   .pipeThrough(new TextDecoderStream())
+   .pipeThrough(new TransformStream(new LineBreakTransformer()))
+   .getReader();
+}
+
+class LineBreakTransformer 
+{
+ constructor() 
+ {
+   this.chunks = "";
+ }
+
+ transform(chunk, controller) 
+ {
+   this.chunks += chunk;
+   const lines = this.chunks.split("\n");
+   this.chunks = lines.pop();
+   lines.forEach((line) => controller.enqueue(line));
+ }
+
+ flush(controller) 
+ {
+   controller.enqueue(this.chunks);
+ }
+}
+
+function joystickDragged()
 {
   strokeWeight(5);
   stroke(x, y, z);
-  if(mouseX >= 50)
+  if(xAxis >= 50)
   {
-    line(mouseX, mouseY, pmouseX, pmouseY);
-    sound1.playbackRate = (mouseY / 200) + 0.001;
+    fill("black");
+    line(xAxis, yAxis, pmouseX, pmouseY);
+    sound1.playbackRate = (green / 200) + 0.001;
     sound1.start();
   }
 }
 
-function mousePressed()
+
+function joystickPressed()
 {
-  if(mouseX <= 50 && mouseY <= 50)
+  if(xAxis <= 50 && yAxis <= 50)
   {
     x = 255, y = 0, z = 0;
     sound2.playbackRate = (mouseY / 200) + 0.001;
     sound2.start();
   }
-  else if(mouseX <= 40 && mouseY > 50 && mouseY < 100)
+  else if(xAxis <= 40 && yAxis > 50 && yAxis < 100)
   {
     x = 255, y = 127, z = 0;
     sound2.playbackRate = (mouseY / 200) + 0.001;
     sound2.start();
   }
-  else if(mouseX <= 40 && mouseY > 100 && mouseY < 150)
+  else if(xAxis <= 40 && yAxis > 100 && yAxis < 150)
   {
     x = 255, y = 255, z = 0;
     sound2.playbackRate = (mouseY / 200) + 0.001;
     sound2.start();
   }
-  else if(mouseX <= 40 && mouseY > 150 && mouseY < 200)
+  else if(xAxis <= 40 && yAxis > 150 && yAxis < 200)
   {
     x = 0, y = 255, z = 0;
     sound2.playbackRate = (mouseY / 200) + 0.001;
     sound2.start();
   }
-  else if(mouseX <= 40 && mouseY > 200 && mouseY < 250)
+  else if(xAxis <= 40 && yAxis > 200 && yAxis < 250)
   {
     x = 0, y = 255, z = 255;
     sound2.playbackRate = (mouseY / 200) + 0.001;
     sound2.start();
   }
-  else if(mouseX <= 40 && mouseY > 250 && mouseY < 300)
+  else if(xAxis <= 40 && yAxis > 250 && yAxis < 300)
   {
     x = 0, y = 0, z = 255;
     sound2.playbackRate = (mouseY / 200) + 0.001;
     sound2.start();
   }
-  else if(mouseX <= 40 && mouseY > 300 && mouseY < 350)
+  else if(xAxis <= 40 && yAxis > 300 && yAxis < 350)
   {
     x = 255, y = 0, z = 255;
     sound2.playbackRate = (mouseY / 200) + 0.001;
     sound2.start();
   }
-  else if(mouseX <= 40 && mouseY > 350 && mouseY < 400)
+  else if(xAxis <= 40 && yAxis > 350 && yAxis < 400)
   {
     x = 150, y = 75, z = 0;
     sound2.playbackRate = (mouseY / 200) + 0.001;
     sound2.start();
   }
-  else if(mouseX <= 40 && mouseY > 400 && mouseY < 450)
+  else if(xAxis <= 40 && yAxis > 400 && yAxis < 450)
   {
     x = 255, y = 255, z = 255;
     sound2.playbackRate = (mouseY / 200) + 0.001;
     sound2.start();
   }
-  else if(mouseX <= 40 && mouseY > 450 && mouseY < 500)
+  else if(xAxis <= 40 && yAxis > 450 && yAxis < 500)
   {
     x = 0, y = 0, z = 0;
     sound2.playbackRate = (mouseY / 200) + 0.001;
